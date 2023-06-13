2c7275d4f0f4b11bc38e37d3c304c28f50e5f1db
JuanCruzHG <juan.cruz.herrera44@gmail.com>
2023-06-13 13:07:45 -0300

    entrega

---
+++ decimal_to_roman.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+
+def decimal_to_roman(decimal):
+    if decimal <= 3:
+        return 'I' * decimal
+    elif decimal == 5:
+        return 'V'
+    else:
+        return "X"
+
+if __name__ == '__main__':
+    unittest.main()
